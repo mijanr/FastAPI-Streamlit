1231b1603ed4897a946a814c593f0f45d41d5467
Md Mijanur Rahman <md.rahman@fau.de>
2023-12-24 12:06:11 +0100

    home pagea dded

---
+++ app/Home.py
@@ -0,0 +1,11 @@
+# src/app.py
+import streamlit as st
+import pandas as pd
+from fastapi import HTTPException
+# welcome message
+st.title("Iris dataset explorer")
+st.write(
+    "This is a simple multi-page Iris dataset explorer app. \
+     It uses a ```FastAPI``` backend to serve the ```Iris``` dataset.\
+     The frontend is built using Streamlit.")
+
