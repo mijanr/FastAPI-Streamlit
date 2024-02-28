c2ed1fe7a6cd46ba36130bf93b60f1be37b2229e
Md Mijanur Rahman <md.rahman@fau.de>
2024-02-28 23:00:40 +0100

    Add Iris prediction endpoint and mount static files directory

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 import pandas as pd
+import joblib
+from typing import List
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/app", StaticFiles(directory="app", html=True), name="app")
 
+
+# get the iris dataset
 @app.get("/iris")
 def get_iris_dataset():
     
@@ -11,3 +18,26 @@ def get_iris_dataset():
     
     return iris.to_dict(orient="records")
 
+# load the pre-trained model
+model = joblib.load("saved_model/saved_model.joblib")
+
+# define the input data model
+class IrisInput(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+# define the prediction endpoint
+@app.post("/predict", response_model=List[float])
+async def predict(data: IrisInput):
+    input_data = [[
+        data.sepal_length,
+        data.sepal_width,
+        data.petal_length,
+        data.petal_width
+    ]]
+    prediction = model.predict(input_data)
+    pred_prob = model.predict_proba(input_data)
+    
+    return prediction, pred_prob.max()*100
